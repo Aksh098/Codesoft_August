815d996848fbddf245574f71effe2046984eee8c
Akshay Patil <114108827+Aksh098@users.noreply.github.com>
2023-08-20 23:40:01 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+first = int(input("Enter first number : "))
+sec = int(input("Enter second number : "))
+print("Addition = ", first+sec)
+print("Substraction = ", first-sec)
+print("Multiplication = ", first*sec)
+print("Division = ", first/sec)
+print("floor division = ", first//sec)
+print("Modulus = ", first%sec)
